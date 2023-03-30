38a36bd515f8464386eca08fd2c49bdcbdebbad3
ganzagauzy <ganzantambuko@gmail.com>
2023-03-30 09:58:12 +0200

    redesign our partners

--- custom.js
+++ custom.js
@@ -25,3 +25,27 @@ button.onclick = function() {
     button.innerHTML = "Show List";
   }
 }
+
+
+$(document).ready(function(){
+  $('.customer-logos').slick({
+      slidesToShow: 6,
+      slidesToScroll: 1,
+      autoplay: true,
+      autoplaySpeed: 1500,
+      arrows: false,
+      dots: false,
+      pauseOnHover:false,
+      responsive: [{
+          breakpoint: 768,
+          setting: {
+              slidesToShow:4
+          }
+      }, {
+          breakpoint: 520,
+          setting: {
+              slidesToShow: 3
+          }
+      }]
+  });
+});

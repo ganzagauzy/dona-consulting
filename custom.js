a6ff0114045d46f43bbbf7631470f8ef6d5beae9
ganzagauzy <ganzantambuko@gmail.com>
2023-03-31 10:19:31 +0200

    our client responsive

--- custom.js
+++ custom.js
@@ -1,4 +1,6 @@
-window.onscroll = function() {scrollFunction()};
+window.onscroll = function () {
+  scrollFunction();
+};
 
 function scrollFunction() {
   if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
@@ -8,15 +10,15 @@ function scrollFunction() {
   }
 }
 
-document.getElementById("back-to-top").onclick = function() {
+document.getElementById("back-to-top").onclick = function () {
   document.body.scrollTop = 0;
   document.documentElement.scrollTop = 0;
-}
+};
 
 var button = document.getElementById("my-button");
 var list = document.getElementById("my-list");
 
-button.onclick = function() {
+button.onclick = function () {
   if (list.style.display === "none") {
     list.style.display = "block";
     button.innerHTML = "Hide List";
@@ -24,28 +26,32 @@ button.onclick = function() {
     list.style.display = "none";
     button.innerHTML = "Show List";
   }
-}
+};
 
-
-$(document).ready(function(){
-  $('.customer-logos').slick({
-      slidesToShow: 6,
-      slidesToScroll: 1,
-      autoplay: true,
-      autoplaySpeed: 1500,
-      arrows: false,
-      dots: false,
-      pauseOnHover:false,
-      responsive: [{
-          breakpoint: 768,
-          setting: {
-              slidesToShow:4
-          }
-      }, {
-          breakpoint: 520,
-          setting: {
-              slidesToShow: 3
-          }
-      }]
+$(document).ready(function () {
+  $(".customer-logos").slick({
+    slidesToShow: 6,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 1500,
+    arrows: false,
+    dots: false,
+    pauseOnHover: true,
+    responsive: [
+      {
+        breakpoint: 768,
+        settings: {
+          slidesToShow: 4,
+          slidesToScroll: 1,
+        },
+      },
+      {
+        breakpoint: 520,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 1,
+        },
+      },
+    ],
   });
 });
